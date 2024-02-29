13514be6ebcc26839641dd1aab3b0d80cfd4ec13
Joel <joelrivera.oa@gmail.com>
2024-02-28 22:40:38 -0600

    Creacion del proyecto

---
+++ Model/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SafeRide.Model
+{
+    internal class Class1
+    {
+    }
+}
---
+++ ViewModel/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SafeRide.ViewModel
+{
+    internal class Class1
+    {
+    }
+}
